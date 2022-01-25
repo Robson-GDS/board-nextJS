7881c03c48091cf64d87b14efd50d5f8b4ba6520
Robson Gomes <robson.080490@gmail.com>
2022-01-25 14:41:03 -0300

    estilizando header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+
+import styles from './styles.module.scss';
+
+export function Header() {
+  return(
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Link href="/">
+          <img src='/images/logo.svg' alt='Logo aplicação' />
+        </Link>
+        <nav> 
+          <Link href="/">
+            <a>Home</a>
+          </Link> 
+          <Link href="/board">
+            <a>Meu board</a>
+          </Link>        
+        </nav>
+
+        <button>
+          Entrar com github
+        </button>
+      </div>
+    </header>
+  )
+}
