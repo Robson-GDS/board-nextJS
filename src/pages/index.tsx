04d4f4095972b89fab9685cedcc4f8f022c4ec76
Robson Gomes <robson.080490@gmail.com>
2022-01-28 17:27:51 -0300

    criando Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,28 @@
 import Head from 'next/head';
 
+import styles from '../styles/styles.module.scss';
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>Board - Organizando sua tarefas.</title>
       </Head>
-      <div>
-        <h1>Teste</h1>
-      </div>
+      <main className={styles.contentContainer}>
+        <img src='/images/board-user.svg' alt='Ferramenta board' />
+
+        <section className={styles.callToAction}>
+          <h1>Uma ferramenta para seu dia a dia Escreva, planeje e organize-se...</h1>
+          <p>
+            <span>100% Gratuita </span>
+            e online.
+          </p>
+        </section>
+
+        <div className={styles.donaters}>
+          <img src='https://avatars.githubusercontent.com/u/77689659?v=4' alt='Usuario 1' />
+        </div>
+      </main>
     </>
   )
 }
