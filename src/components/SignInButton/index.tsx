3be1b5552aa836133a3eff509f1a89f862878b8f
unknown <robson.080490@gmail.com>
2022-02-24 12:49:55 -0300

    teste

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -4,7 +4,7 @@ import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-  const session = true;
+  const session = false;
 
   return session ? (
     <button
