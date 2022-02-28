0cbfa2f5e39d1d702cdc54d9228e99e968d54f08
unknown <robson.080490@gmail.com>
2022-02-28 14:06:51 -0300

    criando NextAuthProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import { SessionProvider as NextAuthProvider } from "next-auth/react"
+
 import { AppProps } from 'next/app';
 import { Header } from '../components/Header';
 
@@ -5,10 +7,10 @@ import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   )
 }
 
