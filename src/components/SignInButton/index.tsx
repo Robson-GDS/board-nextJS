a22b3af14d624fd215f07245d228d079bbca5ff0
unknown <robson.080490@gmail.com>
2022-02-28 14:06:13 -0300

    adicionando login com github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,16 +1,17 @@
+import { signIn, signOut, useSession } from 'next-auth/react'
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-  const session = false;
+  const { data: session } = useSession();
 
   return session ? (
     <button
       type='button'
       className={styles.signInButton}
-      onClick={() => {}}
+      onClick={() => signOut()}
     >
       <img src='https://avatars.githubusercontent.com/u/77689659?v=4' alt='avatar' />
       Olá Robson
@@ -23,7 +24,7 @@ export function SignInButton() {
     <button
       type='button'
       className={styles.signInButton}
-      onClick={() => {}}
+      onClick={() => signIn('github') }
     >
       <FaGithub 
         color='#ffb800'
