b96ff2a843ea42cd4a6bfdbc9d046238f6dc9d1f
unknown <robson.080490@gmail.com>
2022-03-09 14:05:12 -0300

    adicionando callbacks

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -9,6 +9,30 @@ export default NextAuth({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }) as OAuthConfig<Profile>,
-
   ],
+  callbacks: {
+    async session({ session, token }) {
+      try {
+        return {
+          ...session,
+          id: token.sub
+        }
+      }catch{
+        return {
+          ...session,
+          id: null
+        }
+      }
+    },
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+
+      try {
+        return true;
+      }catch(err){
+        console.log('Deu erro!', err)
+        return false
+      }
+    }
+  }
 })
