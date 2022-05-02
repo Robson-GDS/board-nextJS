2e07158fe36ab3c277d15d5f0091cbc531c41e97
unknown <robson.080490@gmail.com>
2022-05-02 06:01:56 -0300

    limitando acesso a pagina detalhes

--- src/pages/board/[id].tsx
+++ src/pages/board/[id].tsx
@@ -1,7 +1,29 @@
+import { GetServerSideProps } from "next"
+import { getSession } from "next-auth/react";
+
 export default function Task() {
   return (
     <div>
       <h1>Pagina detalhes</h1>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req });
+
+  if(!session?.id){
+    return {
+      redirect:{
+        destination: '/board',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {
+
+    }
+  }
 }
