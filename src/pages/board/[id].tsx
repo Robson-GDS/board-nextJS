c2a86aae7ebdf3540b00dddf48fbbb2e057ba5f9
unknown <robson.080490@gmail.com>
2022-05-09 05:23:40 -0300

    configurando pagina detalhes tarefa

--- src/pages/board/[id].tsx
+++ src/pages/board/[id].tsx
@@ -1,8 +1,11 @@
+import Head from "next/head";
 import { GetServerSideProps } from "next";
 import { getSession } from "next-auth/react";
 import { format } from "date-fns";
 
 import firebase from "../../services/firebaseConnection";
+
+import styles from './task.module.scss';
 
 type Task = {
   id: string;
@@ -21,10 +24,11 @@ export default function Task({ data }: TaskListProps) {
   const task = JSON.parse(data) as Task;
 
   return (
-    <div>
-      <h1>Pagina detalhes</h1>
-      <h2>{task.tarefa}</h2>
-    </div>
+    <>
+      <Head>
+        <title>Detalhes da sua tarefa</title>
+      </Head>
+    </>
   )
 }
 
