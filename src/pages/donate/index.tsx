72d10c21949115907645ffd2159118a465e1c920
unknown <robson.080490@gmail.com>
2022-05-27 06:21:33 -0300

    configurando pagamento

--- src/pages/donate/index.tsx
+++ src/pages/donate/index.tsx
@@ -1,6 +1,7 @@
 import { GetServerSideProps } from "next";
 import { getSession } from "next-auth/react";
 import Head from "next/head";
+import { PayPalButtons } from '@paypal/react-paypal-js';
 
 import styles from "./styles.module.scss";
 
@@ -31,6 +32,25 @@ export default function Donate({ user }: DonateProps) {
         <h1>Seja um apoiador desete projeto</h1>
         <h3>Contribua com apenas <span>R$ 1,00</span></h3>
         <strong>Apareça na nossa home, tenha funcionalidades exclusivas.</strong>
+
+        <PayPalButtons 
+          createOrder={(data, actions) => {
+            return actions.order.create({
+              purchase_units: [{
+                amount: {
+                  value: '1'
+                }
+              }]
+            })
+          }}
+
+          onApprove = {(data, actions) => {
+            return actions.order.capture().then(function(details) {
+              console.log('Compra aprovada: ' + details.payer.name.given_name);
+            })
+          }}
+        />
+
       </main>
     </>
   )
